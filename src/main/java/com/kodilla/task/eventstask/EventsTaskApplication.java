0126e486ca7fff63a0b66aa5caf283753c608e13
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-13 18:50:44 +0200

    first commit

---
+++ src/main/java/com/kodilla/task/eventstask/EventsTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.eventstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsTaskApplication.class, args);
+    }
+
+}
